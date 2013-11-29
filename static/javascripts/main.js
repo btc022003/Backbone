bcebbd64219d9a5cbeb3c819d7c02fe9a3c6a1db
btc022003 <btc022003@gmail.com>
2013-11-29 16:55:25 +0800

    绑定model view等...

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -13,9 +13,9 @@ require.config({
 });
 
 require(['json2', 'jquery', 'underscore', 'backbone'],
-    function (JSON, $, _, b) {
+    function (JSON, $, pp, b) {
         // var app = new app();
-        //alert("init");
+       // alert(pp);
         //////////单条新闻记录
         news = b.Model.extend({
             defaults:{
@@ -32,12 +32,36 @@ require(['json2', 'jquery', 'underscore', 'backbone'],
             model:news
          });
 
+        newsView = b.View.extend({
+            template: _.template($('#item_template').html()),
+            initialize: function() {
+                //this.render();
+            },
+            events:{
+                'click a':function(){
+                    alert(this.model.get('title'));
+                },
+                'dblclick p':'dbclickFun' ///////////监听双击事件
+            },
+            render: function() {
+                this.$el.html(this.template(this.model.toJSON()));
+                return this;
+            },
+            dbclickFun:function(e){
+                var p = e.currentTarget; /////////事件的currentTarget  js原来可以和as3一样样的 对事件做这种处理 第一次发现好激动
+                alert($(p).html());
+                //alert(this.model.get('title'));
+            }
+        });
+
         ///////实例化
         var tem_new = new news();
         tem_new.fetch({
             url:"/get_data/177",
             success:function(item,response){
                 console.log(response.title);
+                var view = new newsView({model:item});
+                $("body").append(view.render().el);
             }
         });
     });
